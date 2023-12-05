c3f9fc0e9a01e13872a2f6c8b94aa22f3d29b5a8
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-05 13:26:53 +0200

    changes

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -4,6 +4,8 @@ class GraphEditor {
     this.graph = graph;
     this.ctx = this.canvas.getContext("2d");
 
+    this.selected = null;
+
     this.#addEventListeners();
   }
 
@@ -11,10 +13,14 @@ class GraphEditor {
     this.canvas.addEventListener("mousedown", (evt) => {
       const mouse = new Point(evt.offsetX, evt.offsetY);
       this.graph.addPoint(mouse);
+      this.selected = mouse;
     });
   }
 
   display() {
     this.graph.draw(this.ctx);
+    if (this.selected) {
+      this.selected.draw(this.ctx, { outline: true });
+    }
   }
 }
--- js/primitives/point.js
+++ js/primitives/point.js
@@ -8,11 +8,18 @@ class Point {
     return this.x == point.x && this.y === point.y;
   }
 
-  draw(ctx, size = 18, color = "black") {
+  draw(ctx, { size = 18, color = "black", outline = false } = {}) {
     const rad = size / 2;
     ctx.beginPath();
     ctx.fillStyle = color;
     ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
     ctx.fill();
+    if (outline) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      (ctx.strokeStyle = "Yellow"),
+        ctx.arc(this.x, this.y, rad, 0.6, Math.PI * 2);
+      ctx.stroke();
+    }
   }
 }
