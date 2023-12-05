0573bc1fa80d9cb857c70fec55e61d19cfb57e09
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-05 12:38:06 +0200

    changes

--- js/math/graph.js
+++ js/math/graph.js
@@ -8,6 +8,18 @@ class Graph {
     this.points.push(point);
   }
 
+  containsPoint(point) {
+    return this.points.find((p) => p.equals(point));
+  }
+
+  tryAddPoint(point) {
+    if (!this.containsPoint(point)) {
+      this.addPoint(point);
+      return true;
+    }
+    return false;
+  }
+
   draw(ctx) {
     for (const seg of this.segments) {
       seg.draw(ctx);
--- js/primitives/point.js
+++ js/primitives/point.js
@@ -4,6 +4,10 @@ class Point {
     this.y = y;
   }
 
+  equals(point) {
+    return this.x == point.x && this.y === point.y;
+  }
+
   draw(ctx, size = 18, color = "black") {
     const rad = size / 2;
     ctx.beginPath();
