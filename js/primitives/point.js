ab27bfeb2c5c6e334bb6effb53013d7751b101a5
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-05 10:50:49 +0200

    changes

---
+++ js/primitives/point.js
@@ -0,0 +1,14 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw(ctx, size = 18, color = "black") {
+    const rad = size / 2;
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.arc(this.x, this.y, rad, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
---
+++ js/primitives/segment.js
@@ -0,0 +1,15 @@
+class Segment {
+  constructor(p1, p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+
+  draw(ctx, width = 2, color = "black") {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.moveTo(this.p2.x, this.p2.y);
+    ctx.stroke();
+  }
+}
