c6faa8defdecb418a089ba3f7867eaadabd9613f
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-27 10:24:17 +0200

    changes

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -1,6 +1,35 @@
 class Polygon {
   constructor(points) {
     this.points = points;
+    this.segments = [];
+    for (let i = 1; i <= points.length; i++) {
+      this.segments.push(
+        new Segment(points[i - 1], points[i % points.length])
+      );
+    }
+  }
+
+  static break(poly1, poly2) {
+    const segs1 = poly1.segments;
+    const segs2 = poly2.segments;
+    const intersections = [];
+
+    for (let i = 0; i < segs1.length; i++) {
+      for (let j = 0; j < segs2.length; j++) {
+        const int = getIntersection(
+          segs1[i].p1,
+          segs1[i].p2,
+          segs2[j].p1,
+          segs2[j].p2
+        );
+        if (int && int.offset != 1 && int.offset != 0) {
+          const point = new Point(int.x, int.y);
+          intersections.push(point);
+        }
+      }
+    }
+
+    return intersections;
   }
 
   draw(
--- js/world.js
+++ js/world.js
@@ -14,11 +14,19 @@ class World {
         new Envelope(seg, this.roadWidth, this.roadRoundness)
       );
     }
+
+    this.intersections = Polygon.break(
+      this.envelopes[0].poly,
+      this.envelopes[1].poly
+    );
   }
 
   draw(ctx) {
     for (const env of this.envelopes) {
       env.draw(ctx);
+    }
+    for (const int of this.intersections) {
+      int.draw(ctx, { color: "red", size: 6 });
     }
   }
 }
