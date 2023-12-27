f3a0935ab0e79bb6df7c79abd7cd97eec6b174b5
Raoul Bock <raoulb@deepcatch.com.na>
2023-12-27 11:22:01 +0200

    changes

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -26,8 +26,7 @@ class Envelope {
 
     return new Polygon(points);
   }
-  draw(ctx) {
-    this.poly.draw(ctx);
-    this.poly.drawSegments(ctx);
+  draw(ctx, options) {
+    this.poly.draw(ctx, options);
   }
 }
--- js/world.js
+++ js/world.js
@@ -1,5 +1,5 @@
 class World {
-  constructor(graph, roadWidth = 100, roadRoundness = 3) {
+  constructor(graph, roadWidth = 100, roadRoundness = 10) {
     this.graph = graph;
     this.roadWidth = roadWidth;
     this.roadRoundness = roadRoundness;
@@ -21,9 +21,11 @@ class World {
 
   draw(ctx) {
     for (const env of this.envelopes) {
-      env.draw(ctx);
+      env.draw(ctx, { fill: "#bbb", stroke: "#bbb", lineWidth: 15 });
     }
-
+    for (const seg of this.graph.segments) {
+      seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
+    }
     for (const seg of this.roadBoarders) {
       seg.draw(ctx, { color: "white", width: 4 });
     }
